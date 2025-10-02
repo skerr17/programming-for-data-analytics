76ce694de3dd5ad05cb54d6b66a689c6f7a20622
Stephen Kerr <kerrstephen17@gmail.com>
2025-10-03 00:02:25 +0100

    got the unique northern ireland bankholidays to print but they are for multiple years might update

--- assignments/assignment02-bankholidays.py
+++ assignments/assignment02-bankholidays.py
@@ -8,6 +8,22 @@ url = 'https://www.gov.uk/bank-holidays.json'
 response = requests.get(url)
 data = response.json()
 
+'''
+# Basic print of the events in Northern Ireland
+
 for event in data['northern-ireland']['events']:
     print(f"{event['title']} : {event['date']}")
+
+'''
+
+# Print only the bank holidays that are unique to Northern Ireland
+
+events_in_england_and_wales = [event['title'] for event in data['england-and-wales']['events']]
+events_in_scotland = [event['title'] for event in data['scotland']['events']]
+
+for event in data['northern-ireland']['events']:
+    if event['title'] in events_in_england_and_wales or event['title'] in events_in_scotland:
+        pass
+    else:        
+        print(f"{event['title']} : {event['date']}")
 
